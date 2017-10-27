7d57d37bbcd57108fa39521a1162a724441c6461
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2017-10-27 16:55:44 +0200

    adds init_proxy()

--- R/web.R
+++ R/web.R
@@ -11,7 +11,19 @@ getIEProxy <- function() {
   proxy <- regmatches(syscall, proxy)
   
   proxy <- strsplit(proxy, split = ":", fixed = T)[[1]] # split url and port
+  proxy[2] <- as.integer(proxy[2])
   
   return(proxy)
 }
 
+# set proxy
+init_proxy <- function(proxy = getIEProxy(), ...) {
+  # attach httr
+  if (!("package:httpr" %in% search())) {
+    if (!require(httr)) stop("Cannot load httpr package \n")
+  }
+  
+  if (!is.null(proxy) && proxy != "auto") {
+    httr::set_config(httr::use_proxy(url = proxy[1], port = as.integer(proxy[2]), ...))
+  }
+}
